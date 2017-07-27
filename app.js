6ee4f751c4f5a87435c332bec1f46439729db2d8
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-27 17:30:56 -0400

    Updating

--- app.js
+++ app.js
@@ -36,39 +36,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-// 
-
-app.get('/', function(req, res) {
-  res.render('index', { title: 'Movie Finder' })
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-//
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
-// bind the app to listen for connections on a specified port
-var port = process.env.PORT || 3000;
-app.listen(port);
-
 
 // error handler
 app.use(function(err, req, res, next) {
