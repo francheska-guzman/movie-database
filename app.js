6dcab39fe6a5ad3195217dc08c259df58f49a0eb
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-27 17:10:26 -0400

    Updating

--- app.js
+++ app.js
@@ -36,12 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// 
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Movie Finder' })
@@ -60,6 +55,15 @@ if (app.get('env') === 'development') {
         });
     });
 }
+
+//
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use(function(err, req, res, next) {
