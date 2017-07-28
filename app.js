e219811867f2b62278982fc4cc76da9c15f9fce7
Francheska <francheska-guzman@users.noreply.github.com>
2017-07-27 21:46:44 -0400

    Updating

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.listen(3000, function () {
+console.log("Express server listening on port 3000.");
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
